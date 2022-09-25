85b4e142327131240f50eb8aad03d75f4aef1d5e
Ruhaan <ruhaan313@gmail.com>
2022-09-25 19:43:44 +0500

    function to get computers choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if (randomNumber === 1) {
+        return "Rock";
+    } else if (randomNumber === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    } 
+}
+
+result = getComputerChoice();
+console.log(result)
+
+
+
+
