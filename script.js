8b90b9854ad9d98dc719f7a8877dcf5cddf445c4
Ruhaan <ruhaan313@gmail.com>
2022-09-26 09:32:22 +0500

    created 'play round' function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 function getComputerChoice() {
+
     let randomNumber = Math.floor(Math.random() * 3) + 1;
 
     if (randomNumber === 1) {
@@ -11,8 +12,42 @@ function getComputerChoice() {
     } 
 }
 
-result = getComputerChoice();
-console.log(result)
+function playRound(playerSelection, computerSelection) {
+
+    computerSelection = getComputerChoice();
+    computerSelection = computerSelection.toLowerCase();
+
+    playerSelection = prompt("Make Your Choice");
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        return "Draw";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        return "Draw";
+    } else if (playerSelection === "paper" && computerSelection ==="scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper";
+    } else {
+        return "Draw";
+    }
+}
+
+function game() {
+    
+}
+
+
+
+
 
 
 
